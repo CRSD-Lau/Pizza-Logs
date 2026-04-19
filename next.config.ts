31943df93054e6658c96c8cf9ed0d4886a886e41
Neil Mitchell <neil_mitchell89@hotmail.com>
2026-04-19 11:14:51 -0300

    fix: remove PARSER_SERVICE_URL from build-time env so runtime var is used

--- next.config.ts
+++ next.config.ts
@@ -10,10 +10,6 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [],
   },
-  // Allow the Python parser service URL to be injected at runtime
-  env: {
-    PARSER_SERVICE_URL: process.env.PARSER_SERVICE_URL ?? "http://localhost:8000",
-  },
 };
 
 export default nextConfig;
